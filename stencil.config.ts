a0838398b91d836d395cb2eea6e666c1afde8d39
Gregory Sieranski <git@wonbyte.com>
2021-04-22 13:13:28 -0500

    remove

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,5 +1,4 @@
 import { Config } from '@stencil/core';
-import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'rating-stencil-component',
